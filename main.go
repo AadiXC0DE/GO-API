54eef46f60654e767da9d3aedb75de23302998b1
AadiXC0DE <aadityaz2077@gmail.com>
2023-07-13 15:35:34 +0530

    added returnBook function

--- main.go
+++ main.go
@@ -56,6 +56,23 @@ func bookById(c *gin.Context){
 	c.IndentedJSON(http.StatusOK,book)
 }
 
+func returnBook(c *gin.Context){
+	id,ok :=c.GetQuery("id")
+
+	if !ok{
+		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Missing id query parameter"})
+		return 
+	}
+	book,err :=getBookById(id)
+	if err!=nil{
+		c.IndentedJSON(http.StatusNotFound,gin.H{"message":"Book Not Found"})
+		return
+	}
+	book.Quantity+=1
+	c.IndentedJSON(http.StatusOK,book)
+
+}
+
 func getBookById(id string)(*book,error){
 	for i,b:=range books{
 		if b.ID==id{
@@ -84,5 +101,6 @@ func main(){
 	router.GET("/books/:id",bookById)
 	router.POST("/books",createBook)
 	router.PATCH("/checkout",checkoutBook)
+	router.PATCH("/return",returnBook)
 	router.Run("localhost:3000")
 }
